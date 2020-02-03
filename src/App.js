d7cbcf04bab38c50e822ae31a2574b6e537a12ba
Marco Arteaga <marcoarteaga26@gmail.com>
2020-02-02 20:14:53 -0600

    nav bar links

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import {BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import Col from 'react-bootstrap/Col';
+import Image from 'react-bootstrap/Image';
 import './App.css';
 
-import HomePage from '../src/pages/HomePage';
-import AboutPage from '../src/pages/AboutPage';
-import ContactPage from '../src/pages/ContactPage';
+import Footer from './components/Footer';
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import ContactPage from './pages/ContactPage';
 import PhotoPage from '../src/pages/PhotoPage';
+
 
 class App extends React.Component {
 
@@ -17,8 +25,35 @@ class App extends React.Component {
 
   render(){
     return (
-      <h1>Hello</h1>
-    )
+<Router>
+        <Container className="p-0" fluid={true} style={{backgroundColor: "#b3deff",}}>
+          
+          <Navbar className="border-bottom" bg="transparent" expand="lg" >
+            <Navbar.Brand>
+              {/* Stars and Stripes Drive In */}
+            <Col  className="Logo" xs={6} md={4}>
+              <Image src="https://i.imgur.com/XnA0uWe.jpg" style={{width: '9em', borderRadius: '30%'}} />
+            </Col>
+            </Navbar.Brand>
+
+            <Navbar.Toggle className="border-0" aria-controls="navbar-toggle" />
+            <Navbar.Collapse id="navbar-toggle">
+              <Nav className="ml-auto">
+                <Link className="nav-link" to="/">Home</Link>
+                <Link className="nav-link" to="/about">Movies</Link>
+                <Link className="nav-link" to="/contact">Contact</Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
+
+          <Route path="/" exact render={() => <HomePage title={this.state.home.title} subTitle={this.state.home.subTitle} text={this.state.home.text} />} />
+          <Route path="/about" render={() => <AboutPage title={this.state.about.title} />} />
+          <Route path="/contact" render={() => <ContactPage title={this.state.contact.title} />} />
+          
+          <Footer />
+
+        </Container>
+      </Router>    )
   }
 }
 
